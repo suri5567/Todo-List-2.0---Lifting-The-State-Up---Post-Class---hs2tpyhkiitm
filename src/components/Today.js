31a6f8912b7f9e6e685deb60485654c1ef2811c2
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-03-20 11:33:48 +0530

    Update Today.js

--- src/components/Today.js
+++ src/components/Today.js
@@ -1,29 +1,18 @@
 import React from "react";
 import ListRender from "./ListRender";
-import React from "react";
-import ListRender from "./ListRender";
 
 const Today = (props) => {
   const date = new Date();
-  const filteredList = props.list.filter((task) => {
-    if (date.getFullYear() !== task.date.getFullYear()) {
-      return false;
-    }
-    if (date.getMonth() !== task.date.getMonth()) {
-      return false;
-    }
-    if (date.getDate() !== task.date.getDate()) {
-      return false;
-    }
-    return true;
+  
+  const today = props.list.filter((itm) => {
+    return itm.date === date.toLocaleDateString("en-US");
   });
-
+  
   return (
     <div id="today-list">
-      <ListRender list={filteredList} />
+      <ListRender list={today} />
     </div>
   );
 };
 
 export default Today;
-
