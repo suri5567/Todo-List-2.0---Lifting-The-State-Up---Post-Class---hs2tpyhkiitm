86f7acb39e35329fdd35c156e7323542e01d4f32
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-03-20 11:32:55 +0530

    Update SideNav.js

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -3,9 +3,7 @@ import { FaRegCalendarAlt, FaInbox, FaRegCalendar } from "react-icons/fa";
 import "../styles/App.css";
 
 const SideNav = (props) => {
-
-
-
+  
   return (
     <ul className="side-nav">
       <li onClick= {() => {props.change('INBOX')}}>
@@ -25,4 +23,3 @@ const SideNav = (props) => {
 };
 
 export default SideNav;
-
